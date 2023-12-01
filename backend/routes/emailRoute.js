18c25549670cfe8c5f8e6f6f7a7357f02f6e93cc
Ieva <121813092+IevaZR@users.noreply.github.com>
2023-12-01 12:12:18 +0200

    Delete backend directory

--- backend/routes/emailRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addEmail } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-router.post("/add-email", addEmail);
-
-export default router;
